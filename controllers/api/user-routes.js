54d59068059387cbf47c130572cf696e498b1c51
joe-white09 <joe_white09@yahoo.com>
2022-07-18 14:07:19 -0500

    added update and delete functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Get /api/users
 router.get('/', (req, res) => {
@@ -56,7 +57,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {username: '', email: '', password: ''}
     User.create({
         username: req.body.username,
@@ -116,7 +117,7 @@ router.post('/logout', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // expects { username: '', email: '', password: ''}
 
     // if req.body has exact key/value pairs to match the model, you can just use 'req.body' instead
@@ -140,7 +141,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
